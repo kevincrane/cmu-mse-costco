7b70d433833b875461a7319a0c4471e55ed8a438
A.Samiyev <A.Samiyev@128.237.134.195>
2013-02-07 18:35:59 -0500

    Removed Notification related code from ViewListActitity and put it in a separate NotificationAvtivity

---
+++ shoppinglist/src/cmu/costco/shoppinglist/NotificationActivity.java
@@ -0,0 +1,153 @@
+package cmu.costco.shoppinglist;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Notification activity
+ * @author A.Samiyev
+ *
+ */
+public class NotificationActivity extends Activity implements LocationListener {
+
+	private LocationManager locationManager=null;
+	
+	private EditText lat;
+	private EditText lon;
+	private EditText alertName;
+	
+	
+	private ListView alertList;
+	private ArrayList<String> list;
+	private ArrayAdapter<String> adapter;
+	
+	
+	private Map<String,List<Double>> poi;
+	
+	private final float RADIUS = 2;
+	private final long EXPIRATION = -1;
+	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        
+        poi = new HashMap<String,List<Double>>();
+        
+        alertList = (ListView)findViewById(R.id.alertList);
+        list = new ArrayList<String>();
+    	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+    	alertList.setAdapter(adapter);
+    	
+    	//Register the listener with the Location Manager to receive location updates
+		locationManager =
+		        (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		try {
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @Override
+	public void onPause() {
+		super.onPause();
+		if(locationManager!=null)
+		{
+			locationManager.removeUpdates(this);
+			locationManager = null;
+		}
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_notification, menu);
+        return true;
+    }
+    
+    
+public boolean alertOnClick(View v) {
+    	
+           
+    	List<Double> coordinates = new ArrayList<Double>();
+    	
+    	this.lat = (EditText)findViewById(R.id.Latitude);		
+		this.lon = (EditText)findViewById(R.id.Longitude);
+		this.alertName = (EditText)findViewById(R.id.alertName);
+		
+		// check if alertName already exists
+		if(poi.containsKey(alertName.getText().toString())) {
+			Toast.makeText(this, "Name already exists", Toast.LENGTH_LONG).show();
+			return false;
+		}
+    	
+    	coordinates.add(Double.valueOf(this.lat.getText().toString()));
+    	coordinates.add(Double.valueOf(this.lon.getText().toString()));
+    	
+    	poi.put(this.alertName.getText().toString(), coordinates);
+    	
+    	PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(this.alertName.getText().toString()), 0);
+    	locationManager.addProximityAlert(coordinates.get(0), coordinates.get(1), RADIUS, EXPIRATION, pendingIntent);
+
+    	IntentFilter intentFilter = new IntentFilter(this.alertName.getText().toString());
+    	registerReceiver(new ProximityIntentReceiver(),intentFilter);
+    	  
+   	
+    	list.add(this.alertName.getText().toString());
+    	adapter.notifyDataSetChanged();
+
+    	return false;
+    }
+
+
+
+	@Override
+	public void onLocationChanged(Location location) 
+	{
+		// Get GPS coordinates
+		lat = (EditText)findViewById(R.id.Latitude);		
+		lon = (EditText)findViewById(R.id.Longitude);
+		
+		lat.setText(Double.toString(location.getLatitude()));
+		lon.setText(Double.toString(location.getLongitude()));        	
+	}
+	
+	@Override
+	public void onProviderDisabled(String provider) 
+	{
+		locationManager.removeUpdates(this);
+	}
+	
+	@Override
+	public void onProviderEnabled(String provider) 
+	{  
+		
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) 
+	{       
+	}
+    
+    
+}
--- shoppinglist/src/cmu/costco/shoppinglist/ViewListActivity.java
+++ shoppinglist/src/cmu/costco/shoppinglist/ViewListActivity.java
@@ -1,60 +1,34 @@
 package cmu.costco.shoppinglist;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Paint;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 import cmu.costco.shoppinglist.db.DatabaseAdaptor;
 import cmu.costco.shoppinglist.objects.Customer;
 import cmu.costco.shoppinglist.objects.ShoppingListItem;
 
-public class ViewListActivity extends Activity implements LocationListener {
+public class ViewListActivity extends Activity  {
 
 	private final static String TAG = "ViewListActivity";
 
 	private DatabaseAdaptor db;
 	private Customer cust;
 	
-	private EditText lat;
-	private EditText lon;
-	private EditText alertName;
-	
-	private Map<String,List<Double>> poi; 
-	
-
-	private LocationManager locationManager=null;
-	
-	private final float RADIUS = 2;
-	private final long EXPIRATION = -1;
-	
-	private ArrayList<String> list;
-	private ArrayAdapter<String> adapter;
-	private ListView alertList;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -79,21 +53,8 @@ public class ViewListActivity extends Activity implements LocationListener {
 		ScrollView scroll = (ScrollView) findViewById(R.id.viewListScroll);
 		LinearLayout itemList = generateListView(this, cust.getShoppingList());
 		scroll.addView(itemList);
-		
-		poi = new HashMap<String,List<Double>>();
-		
 	}
 	
-	@Override
-	public void onPause() {
-		super.onPause();
-		if(locationManager!=null)
-		{
-			locationManager.removeUpdates(this);
-			locationManager = null;
-		}
-	}
-
 	@Override
 	public void onResume() {
 		super.onResume();
@@ -205,105 +166,12 @@ public class ViewListActivity extends Activity implements LocationListener {
 		super.onOptionsItemSelected(item);
 
 		if (item.getItemId() == R.id.AddProximityAlert) {
-			setContentView(R.layout.proximityalert);
-			
-	    	alertList = (ListView)findViewById(R.id.alertList);
-	    	list = new ArrayList<String>();
-	    	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-	    	alertList.setAdapter(adapter);
-			
-            //Register the listener with the Location Manager to receive location updates
-			locationManager =
-			        (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-			try {
-				locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
+			Intent intent = new Intent(this, NotificationActivity.class);
+			startActivity(intent);
 		}
 		
 		return true;
 	}
-	
-    public boolean alertOnClick(View v) {
-    	
-//    	NotificationManager notificationManager = 
-//                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-//            Notification notification = createNotification();
-//            
-//            Intent intent = new Intent(ViewListActivity.this, ViewListActivity.class);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-//            
-//        
-//            notification.setLatestEventInfo(this, 
-//            		"Test", "You are entering your point of interest.", pendingIntent);
-//            notificationManager.notify(1, notification);
-            
-    	List<Double> coordinates = new ArrayList<Double>();
-    	
-    	this.lat = (EditText)findViewById(R.id.Latitude);		
-		this.lon = (EditText)findViewById(R.id.Longitude);
-		this.alertName = (EditText)findViewById(R.id.alertName);
-		
-		// check if alertName already exists
-		if(poi.containsKey(alertName.getText().toString())) {
-			Toast.makeText(this, "Name already exists", Toast.LENGTH_LONG).show();
-			return false;
-		}
-    	
-    	coordinates.add(Double.valueOf(this.lat.getText().toString()));
-    	coordinates.add(Double.valueOf(this.lon.getText().toString()));
-    	
-    	poi.put(this.alertName.getText().toString(), coordinates);
-    	
-    	PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(this.alertName.getText().toString()), 0);
-    	locationManager.addProximityAlert(coordinates.get(0), coordinates.get(1), RADIUS, EXPIRATION, pendingIntent);
-
-    	IntentFilter intentFilter = new IntentFilter(this.alertName.getText().toString());
-    	registerReceiver(new ProximityIntentReceiver(),intentFilter);
-    	  
-//    	alertList = (ListView)findViewById(R.id.alertList);
-//    	list = new ArrayList<String>();
-//    	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-//    	alertList.setAdapter(adapter);
-    	
-    	list.add(this.alertName.getText().toString());
-    	adapter.notifyDataSetChanged();
-
-    	return false;
-    }
-    
-
-    
-    @Override
-    public void onLocationChanged(Location location) 
-    {
-		// Get GPS coordinates
-		lat = (EditText)findViewById(R.id.Latitude);		
-		lon = (EditText)findViewById(R.id.Longitude);
-		
-		lat.setText(Double.toString(location.getLatitude()));
-		lon.setText(Double.toString(location.getLongitude()));        	
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) 
-    {
-    	locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) 
-    {  
-    	
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) 
-    {       
-    }
-
     
     
 }
